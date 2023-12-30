07e4491ca7d56b0564387598f5aa4e4846ba254a
Joe Scharnitzke <j.scharnitzke@gmail.com>
2023-12-30 15:23:46 -0500

    add foundational layout elements

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ContentBody()
                 }
             }
         }
@@ -30,17 +33,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ContentBody(modifier: Modifier = Modifier) {
+    Column(modifier =  modifier) {
+        Row(modifier = modifier.weight(weight = 0.5F)) {
+            TextComposable(modifier = modifier)
+        }
+        Row(modifier = modifier.weight(weight = 0.5F)) {
+
+        }
+    }
+}
+
+@Composable
+fun TextComposable(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = stringResource(id = R.string.text_composable_title),
         modifier = modifier
+    )
+    Text(
+        text = stringResource(id = R.string.text_composable_content)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeQuadrantPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ContentBody()
     }
 }
